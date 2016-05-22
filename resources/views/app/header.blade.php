e99310313ca704ee266200397163a35ffe80380e
yantao <a1292717155@gmail.com>
2016-05-22 04:24:34 +0200

    a119347

--- resources/views/app/header.blade.php
+++ resources/views/app/header.blade.php
@@ -22,14 +22,24 @@
     </ul>
 
 </div>
-<ul class="list-unstyled" id="drop-bar">
-    @foreach($products as $product)
+<div style="width: 100%;" id="drop-bar">
+    <ul class="list-unstyled" style="position: relative;">
+        @foreach($products as $product)
         <li class="drop-bar-item">
-            <img src="{{URL::asset('/storage/uploads/'.$product->productImage)}}" alt="{{$product->name}}" style="width: 119px;height: 68px;display: block;margin: 0px auto">
-            <p>{{$product->name}}</p>
+        <img src="{{URL::asset('/storage/uploads/'.$product->productImage)}}" alt="{{$product->name}}" style="width: 119px;height: 68px;display: block;margin: 0px auto">
+        <p>{{$product->name}}1</p>
         </li>
-    @endforeach
-</ul>
+        @endforeach
+        {{--@for($i=0;$i<13;$i++)--}}
+            {{--<li class="drop-bar-item" style="position: absolute;left:{{165*($i+1)}}px">--}}
+                {{--<img src="/images/menu-saumon-xl.png"  style="width: 119px;height: 68px;display: block;margin: 0px auto">--}}
+                {{--<p>MENU LUNCH {{$i}}</p>--}}
+            {{--</li>--}}
+        {{--@endfor--}}
+
+    </ul>
+
+</div>
 <script>
     $('#show-drop-bar').hover(function () {
         $('ul#drop-bar').stop().fadeIn(500);
@@ -45,7 +55,7 @@
     });
     $('.drop-bar-item').hover(function () {
         var index = $(".drop-bar-item").index(this);
-        var left=(170-60*index)+'px';
-        $('.drop-bar-item').stop().animate({left:left},10);
+        var left = (-110*index)+'px';
+        $('ul.list-unstyled').stop().animate({left:left},150);
     })
 </script>
