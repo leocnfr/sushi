5c71629581ee512507f08389d6e6c77301d3664f
yantao <a1292717155@gmail.com>
2016-05-22 21:46:40 +0200

    a119347

--- resources/views/app/modal.blade.php
+++ resources/views/app/modal.blade.php
@@ -47,10 +47,7 @@
                     </div>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Send message</button>
-            </div>
+           
         </div>
     </div>
 </div>
