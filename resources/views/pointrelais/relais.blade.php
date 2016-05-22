e46c3cecaef472bdef18015042c972f04dd664a1
yantao <a1292717155@gmail.com>
2016-05-22 13:41:13 +0200

    a119347

--- resources/views/pointrelais/relais.blade.php
+++ resources/views/pointrelais/relais.blade.php
@@ -2,6 +2,10 @@
 @section('page_title','Relais列表')
 @section('relaisactive','active')
 @section('content')
+
+    <div id="map">
+
+    </div>
     <table class="table table-hover">
     	<thead>
     		<tr>
@@ -31,4 +35,21 @@
             @endforeach
     	</tbody>
     </table>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAnx6xBL6gsxf5NQIHoHALwIeAGLmeXqu8&signed_in=true&callback=initMap"></script>
+    <script>
+        function initMap() {
+            var myLatLng = {lat: -25.363, lng: 131.044};
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 4,
+                center: myLatLng
+            });
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: 'Hello World!'
+            });
+        }
+    </script>
 @endsection
