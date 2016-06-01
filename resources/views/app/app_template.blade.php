85622e2ed2bd54934f34faab6e74a64e4ffdfcd9
yantao <a1292717155@gmail.com>
2016-06-01 22:52:41 +0200

    a119347

--- resources/views/app/app_template.blade.php
+++ resources/views/app/app_template.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Lunch Sushi</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <link href="{{URL::asset("css/bootstrap.css")}}" rel="stylesheet" type="text/css" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
